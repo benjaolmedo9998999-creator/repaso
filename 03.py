d594c6506b1af5c530e2b13ddc717af1ff6e4eba
benjaolmedo9998999-creator <benjaolmedo9998999@gmail.com>
2026-03-05 12:49:18 -0300

    Add for and while loops with print statements

--- 03.py
+++ 03.py
@@ -1,1 +1,12 @@
+print("--- Inicio del ciclo FOR ---")
+for i in range(1, 6):
+    print("Número de vuelta:", i)
+print("¡Ciclo terminado!")
 
+
+print("--- Inicio del ciclo WHILE ---")
+contador = 5
+while contador > 0:
+    print("Despegue en:", contador)
+    contador = contador - 1 
+print("FUEGO")
