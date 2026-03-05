37cf48ff6f3ad078cde5f80a612483d4c3579300
benjaolmedo9998999-creator <benjaolmedo9998999@gmail.com>
2026-03-05 12:44:55 -0300

    Update 02.py

--- 02.py
+++ 02.py
@@ -1,1 +1,9 @@
+edad = 17
+if edad >= 18:
+    print("✅ Podés pasar, sos mayor de edad.")
+    print("Disfrutá la joda.")
+else:
+    print("❌ No podés pasar.")
+    print("Tenés que venir con un adulto o esperar unos años.")
 
+print("--- Fin ---")
