c4e7c07e604c0f2d30633cc3517dc8bd63b7db50
benjaolmedo9998999-creator <benjaolmedo9998999@gmail.com>
2026-03-05 12:35:18 -0300

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,1 +1,7 @@
-
+numero1 = 10
+numero2 = 20
+resultado = numero1 + numero2
+print("El primer número es:", numero1)
+print("El segundo número es:", numero2)
+print("")
+print("La suma total es:", resultado)
